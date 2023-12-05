3638cb3bdd8ac9be4deb3364e96d0136601f8c5b
Daniel Soares Caldeira <soarescaldeira@outlook.com>
2023-12-04 23:55:58 -0300

    Gitacora saved 1 changed files

--- assets/default/carousel.js
+++ assets/default/carousel.js
@@ -1,17 +1,18 @@
 
-for (var i=1; i<minPerSlide; i++) {
-let items = document.querySelectorAll('#servicos .carousel .carousel-item')
+for (var i = 1; i < minPerSlide; i++) {
+    let items = document.querySelectorAll('#servicos .carousel .carousel-item')
 
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
+    items.forEach((el) => {
+        const minPerSlide = 4
+        let next = el.nextElementSibling
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+}
