c415f540659fef458b797a7c43a12aebb13ed534
Daniel Soares Caldeira <soarescaldeira@outlook.com>
2023-12-05 00:06:57 -0300

    Gitacora saved 1 changed files

--- assets/default/carousel.js
+++ assets/default/carousel.js
@@ -1,7 +1,7 @@
 
 for (var j = 0; j < 5; j++) {
     let css = "#servicos #recipeCarousel" + j + " .carousel .carousel-item";
-    let items = document.querySelectorAll(css)
+    let items = document.querySelectorAll(css);
     console.log(css)
     console.log(items)
     items.forEach((el) => {
